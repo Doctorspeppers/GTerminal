954d0c9118bf3b49ac8c4115e20ced5e63f650bf
Doctorspeppers <Doctorspeppers@gmail.com>
2018-11-12 23:22:20 -0200

    Version 1.2

--- GTerminal.py
+++ GTerminal.py
@@ -3,12 +3,13 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 cont = 0
-search = input("Escreva sua pesquisa:")
-resultado_quant = input("Quantos resultados você quer?\nEscreva um numero:")
-path = input("Onde deseja salvar o arquivo?Escreva um local ou nome:")
+search = raw_input("Escreva sua pesquisa:")
+print(search)
+resultado_quant = raw_input("Quantos resultados você quer?\nEscreva um numero:")
+path = raw_input("Onde deseja salvar o arquivo?Escreva um local ou nome:")
 with open('caracteres.csv') as f:
 	f_csv = csv.DictReader(f)
-	for row in f_csv:		
+	for row in f_csv:
 		search = search.replace(row['simb'], row['ASC'])
 print(search)
 numlinks = 0
@@ -16,7 +17,7 @@ numlinks = 0
 trabalho = True
 while trabalho == True:
 	cont = cont+10
-	page = requests.get("https://www.google.com.br/search?q="+search+"&oq="+search+"&start="+str(cont))
+	page = req = requests.request('GET', "https://www.google.com.br/search?q="+search+"&oq="+search+"&start="+str(cont))
 	soup = BeautifulSoup(page.content, 'html.parser')
 	print(page.status_code)
 	resultado = soup.find_all('h3', class_='r')
@@ -27,6 +28,7 @@ while trabalho == True:
 		x = x.replace('/url?q=', "")
 		x1 = x.split("&amp", 1)
 		x2 = x1[1].split(">", 1)
+		print(x1, x2)
 		x = x1[0]+"\">"+x2[1]
 		arquivo.write(x)
 		numlinks = numlinks+1
